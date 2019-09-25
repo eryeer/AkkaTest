b9b3186967f5e094c5009b8d4f8915725f1543fd
Charis <eryeer@163.com>
2019-09-25 15:00:14 +0800

    init

---
+++ Study/Study/Lifecycle.cs
@@ -0,0 +1,47 @@
+﻿using Akka.Actor;
+using System;
+
+namespace Study
+{
+    public class Lifecycle
+    {
+        static void Main(string[] args)
+        {
+            var sys = ActorSystem.Create("MySystem");
+            var firstRef = sys.ActorOf(Props.Create<StartStopActor1>(), "first");
+            firstRef.Tell("stop");
+            Console.ReadLine();
+        }
+    }
+
+    public class StartStopActor1 : UntypedActor
+    {
+        protected override void PreStart()
+        {
+            Console.WriteLine("first started");
+            Context.ActorOf(Props.Create<StartStopActor2>(), "second");
+        }
+
+        protected override void PostStop() => Console.WriteLine("first stopped");
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case "stop":
+                    Context.Stop(Self);
+                    break;
+            }
+        }
+    }
+
+    public class StartStopActor2 : UntypedActor
+    {
+        protected override void PreStart() => Console.WriteLine("second started");
+        protected override void PostStop() => Console.WriteLine("second stopped");
+
+        protected override void OnReceive(object message)
+        {
+        }
+    }
+}
---
+++ Study/Study/Program.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using System;
+
+namespace Study
+{
+    public class Program
+    {
+        static void Main01(string[] args)
+        {
+            var sys = ActorSystem.Create("MySystem");
+            var firstRef = sys.ActorOf(Props.Create<PrintMyActorRefActor>(), "first-actor");
+            Console.WriteLine($"First: {firstRef}");
+            firstRef.Tell("printit", ActorRefs.NoSender);
+            Console.ReadLine();
+        }
+    }
+
+    public class PrintMyActorRefActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case "printit":
+                    IActorRef secondRef = Context.ActorOf(Props.Empty, "second-actor");
+                    Console.WriteLine($"Second: {secondRef}");
+                    break;
+            }
+        }
+    }
+}
