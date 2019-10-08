4b610754ca66186607e700f0ea2066db5219fb70
Charis <eryeer@163.com>
2019-10-08 16:21:35 +0800

    add test

---
+++ Study/AkkaServerMultiActorSinglePort/Program.cs
@@ -0,0 +1,97 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using Akka.IO;
+using System;
+using System.Net;
+using System.Text;
+
+namespace AkkaServerMultiActorSinglePort
+{
+    public class StartServer
+    {
+        static void Main(string[] args)
+        {
+            var sys = ActorSystem.Create("ServerSystem");
+            var server1 = sys.ActorOf(TcpServer.Props(8088), "tcp-server1");
+            Console.ReadLine();
+        }
+    }
+
+    public class TcpServer : UntypedActor
+    {
+        protected ILoggingAdapter Log { get; } = Context.GetLogger();
+        public static Props Props(int port) => Akka.Actor.Props.Create(() => new TcpServer(port));
+
+        public TcpServer(int port)
+        {
+            Context.System.Tcp().Tell(new Tcp.Bind(Self, new IPEndPoint(IPAddress.Any, port)));
+            Log.Info($"start to listen port {port}");
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Tcp.Bound)
+            {
+                var bound = message as Tcp.Bound;
+                Log.Info("Listening on {0}", bound.LocalAddress);
+            }
+            else if (message is Tcp.Connected)
+            {
+                var connectMsg = message as Tcp.Connected;
+                var connection = Context.ActorOf(Akka.Actor.Props.Create(() => new EchoConnection(Sender)));
+                Sender.Tell(new Tcp.Register(connection));
+                Log.Info($"{connection.ToString()} connected to {connectMsg.RemoteAddress}");
+            }
+            else Unhandled(message);
+        }
+    }
+
+    public class EchoConnection : UntypedActor
+    {
+        private long receivedCount = 0;
+        protected ILoggingAdapter Log { get; } = Context.GetLogger();
+        private readonly IActorRef _connection;
+        private ICancelable scheduler;
+
+        public EchoConnection(IActorRef connection)
+        {
+            _connection = connection;
+            scheduler = Context.System.Scheduler.ScheduleTellRepeatedlyCancelable(1000, 1000, Self, new TPSTimer(), Self);
+        }
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case Tcp.Received received:
+                    if ("stop_server".Equals(Encoding.ASCII.GetString(received.Data.ToArray())))
+                        Context.Stop(Self);
+                    else
+                    {
+                        receivedCount++;
+                        //Log.Info($"{receivedCount} Data received");
+                        //for (int i = 0; i <= 100000; i++)
+                        //{ }
+                        //Log.Info("msg size is {0}", received.Data.Count);
+                        //var arr = received.Data.ToArray().ToHexString();
+                        //Log.Info("msg content: {0}", arr);
+                    }
+                    break;
+                case TPSTimer _:
+                    Log.Info("Tps is {0}/s", receivedCount);
+                    receivedCount = 0;
+                    break;
+                case Tcp.ConnectionClosed _:
+                    Log.Warning("Connection Closed");
+                    scheduler.CancelIfNotNull();
+                    Context.Stop(Self);
+                    break;
+                default:
+                    Unhandled(message);
+                    break;
+            }
+        }
+    }
+
+    internal class TPSTimer { }
+}
